1672062cb60b1290e6a3e3c0e657c4267c9a736e
wasayabdul <wasay54@hotmail.com>
2025-11-13 22:00:17 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,3 +6,27 @@ import os
 
 st.set_page_config(page_title="Simple Finance App",
                    page_icon="💰", layout="wide")
+
+
+def load_transactions(file):
+    try:
+        df = pd.read_csv(file)
+        st.write(df)
+        return df
+
+    except Exception as e:
+        st.error(f"Error processing file: {str(e)}")
+        return None
+
+
+def main():
+    st.title("Simple Finance Dashboard")
+
+    uploaded_file = st.file_uploader(
+        "Upload your transaction CSV file:", type=["csv"])
+
+    if uploaded_file is not None:
+        df = load_transactions(uploaded_file)
+
+
+main()
