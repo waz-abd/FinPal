f416cb22aa78dfc42ab400267115be71bbcb5c23
wasayabdul <wasay54@hotmail.com>
2025-11-13 22:22:15 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,11 +7,20 @@ import os
 st.set_page_config(page_title="Simple Finance App",
                    page_icon="💰", layout="wide")
 
+if "categories" not in st.session_state:
+    st.session_state.categories = {
+        "Uncategorized": []
+    }
+
+if os.path.exists("categories.json")
+
 
 def load_transactions(file):
     try:
         df = pd.read_csv(file)
-        st.write(df)
+        df.columns = [col.strip() for col in df.columns]
+        df["Amount"] = df["Amount"].str.replace(",", "").astype(float)
+        df["Date"] = pd.to_datetime(df["Date"], format="%d %b %Y")
         return df
 
     except Exception as e:
@@ -27,6 +36,18 @@ def main():
 
     if uploaded_file is not None:
         df = load_transactions(uploaded_file)
+
+        if df is not None:
+            debits_df = df[df["Debit/Credit"] == "Debit"].copy()
+            credits_df = df[df["Debit/Credit"] == "Credit"].copy()
+
+            tab1, tab2 = st.tabs(["Expenses (Debits)", "Payments (Credits)"])
+
+            with tab1:
+                st.write(debits_df)
+
+            with tab2:
+                st.write(credits_df)
 
 
 main()
